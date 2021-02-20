965f1c7e35cea8811bee44354a484f8017c72af1
rabsef-bicrym <eric.helal@protonmail.com>
2021-02-20 01:34:25 +0000

    Adding app hosting

---
+++ home-react-full/app/todoreact/precache-manifest.d8879c74fc1a4a422c8d09e25f321cf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1dc9c6f50fbb1c56c5b6",
+    "url": "/~todoreact/static/js/runtime~main.1dc9c6f5.js"
+  },
+  {
+    "revision": "22949569891075b9302b",
+    "url": "/~todoreact/static/js/main.22949569.chunk.js"
+  },
+  {
+    "revision": "f6a93fda9068c9bbb019",
+    "url": "/~todoreact/static/js/2.f6a93fda.chunk.js"
+  },
+  {
+    "revision": "22949569891075b9302b",
+    "url": "/~todoreact/static/css/main.be9cd952.chunk.css"
+  },
+  {
+    "revision": "f6a93fda9068c9bbb019",
+    "url": "/~todoreact/static/css/2.5ec1470f.chunk.css"
+  },
+  {
+    "revision": "547c018b1c899ed7c36703407ff3be3a",
+    "url": "/~todoreact/index.html"
+  }
+];
